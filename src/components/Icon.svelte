292b0f9c19a3432651cfc4304e1d57db0e98446d
Jo Riviera <guirito.jc@gmail.com>
2021-11-29 20:06:20 +0100

    ADD Icon svelte component

---
+++ src/components/Icon.svelte
@@ -0,0 +1,61 @@
+<script>
+  /**
+   * Classes icon wrapper.
+   */
+  let klass = '';
+  export { klass as class };
+  /**
+   * Size of the icon.
+   */
+  export let size = '1em';
+  /**
+   * ViewBox if `path` value is present.
+   */
+  export let viewBox = '0 0 24 24';
+  /**
+   * If `true`, the icon will be spinning.
+   */
+  export let spin = false;
+  /**
+   * If `true`, the icon will be disabled.
+   */
+  export let disabled = false;
+  /**
+   * An svg path.
+   */
+  export let path = null;
+
+  export let fillColor;
+
+  export let stroke;
+</script>
+
+<style>
+  .is-spinning {
+
+  }
+
+  .icon-disabled {
+
+  }
+
+</style>
+
+<i
+  aria-hidden="true"
+  class="{klass}"
+  class:is-spinning={spin}
+  class:icon-disabled={disabled}
+  style="font-size:{size};">
+  {#if path}
+    <svg xmlns="http://www.w3.org/2000/svg"
+         width={size} height={size}
+         {viewBox}
+        style="fill: {fillColor || 'currentColor'};
+          stroke: {stroke || 'currentColor'};"
+    >
+      <path d={path} />
+    </svg>
+  {/if}
+  <slot />
+</i>
